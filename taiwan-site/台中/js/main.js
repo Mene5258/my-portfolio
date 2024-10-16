281f922332e4c95f1157372a22ac93d180a1affd
Mene5258 <irukashou@gmail.com>
2024-10-16 15:09:14 +0900

    Add files via upload

---
+++ taiwan-site/台中/js/main.js
@@ -0,0 +1,56 @@
+'use strict'
+
+// モーダル1
+const dialog1 = document.getElementById('dialog1');
+
+const openbtn1 = document.getElementById('openbtn1');
+
+openbtn1.onclick = function () {
+  dialog1.show();
+};
+
+const closebtn1 = document.getElementById('closebtn1');
+closebtn1.onclick = function () {
+  dialog1.close();
+};
+
+// モーダル２
+
+const dialog2 = document.getElementById('dialog2');
+const openbtn2 = document.getElementById('openbtn2');
+
+openbtn2.onclick = function () {
+  dialog2.show();
+};
+
+const closebtn2 = document.getElementById('closebtn2');
+closebtn2.onclick = function () {
+  dialog2.close();
+};
+
+// モーダル3
+const dialog3 = document.getElementById('dialog3');
+const openbtn3 = document.getElementById('openbtn3');
+
+openbtn3.onclick = function () {
+  dialog3.show();
+};
+
+const closebtn3 = document.getElementById('closebtn3');
+closebtn3.onclick = function () {
+  dialog3.close();
+};
+
+// モーダル4
+
+const dialog4 = document.getElementById('dialog4');
+const openbtn4 = document.getElementById('openbtn4');
+
+openbtn4.onclick = function () {
+  dialog4.show();
+};
+
+const closebtn4 = document.getElementById('closebtn4');
+closebtn4.onclick = function () {
+  dialog4.close();
+};
